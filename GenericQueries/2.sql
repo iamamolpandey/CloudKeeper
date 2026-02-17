3db889e7c8d12cb2cdb61365d4dc4ea96563095d
AMOL KUMAR PANDEY <87844443+iamamolpandey@users.noreply.github.com>
2026-02-17 12:36:57 +0530

    SQL query for data transfer costs
    Updated SQL query to use a new database and table name.

---
+++ GenericQueries/2.sql
@@ -0,0 +1,38 @@
+-- 2. For all region get Data Transfer Costs on a daily basis, separate file per region = dt_costs
+
+SELECT 
+
+product_servicecode, 
+
+-- If want to see service wise Data Transfer
+-- line_item_product_code,
+
+-- If need Daily Granularity
+-- DATE(at_timezone(line_item_usage_start_date,'UTC')) AS usage_date,
+
+-- If need Group By Name Tag
+-- COALESCE(
+--     NULLIF(resource_tags_user_name, ''),
+--     'UNTAGGED'
+-- ) AS tag_name,
+
+ROUND(SUM(line_item_unblended_cost),2) AS cost
+
+FROM "db_name"."table_name"
+WHERE 
+
+product_servicecode = 'AWSDataTransfer'
+
+-- Region filter (change if needed)
+-- AND product_region_code = 'ap-south-1'
+
+-- Only 2 accounts
+-- AND line_item_usage_account_id IN (
+--     '65***************',
+--     '65***************'
+-- )
+
+AND DATE(at_timezone(line_item_usage_start_date,'UTC')) BETWEEN DATE '2026-01-01' AND DATE '2026-01-31'
+
+GROUP BY 1
+;
